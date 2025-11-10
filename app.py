9696c417213c2f896c1c7a6ab927016e873a5c6c
Irinka-koz <kambat@gmail.com>
2025-11-10 10:08:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -373,7 +373,7 @@ with col_right:
         {"Sensor ID": k, "Area":v["Area"], "Location": v["Location"], "Type": v["Type"]}
         for k, v in sensor_info.items()
     ])
-    st.dataframe(sensor_info_table, use_container_width=True, height=150)
+    st.dataframe(sensor_info_table, use_container_width=True, height=400)
 
 # ----------------------------
 # HEATMAP
@@ -381,6 +381,7 @@ with col_right:
 st.markdown("---")
 st.header("Sensor Maintenance Calendar")
 build_heatmap(df)
+
 
 
 
