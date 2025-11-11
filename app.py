d14fb0dd1ad5fd683913622a0c0b9c5b99ce158f
Irinka-koz <kambat@gmail.com>
2025-11-11 12:25:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def build_heatmap(df):
         2: '#FF3333',  # Battery
         3: '#FF9900',  # Card
         4: '#800080',   # Both
-        5: '#3399FF' #Location
+        5: '#3399FF', #Location
         6: '#FCDC4D', #Manual Count
         7:'#D496A7', #Other
     }
@@ -424,6 +424,7 @@ with col_right:
 st.markdown("---")
 st.header("Sensor Maintenance Calendar")
 build_heatmap(df)
+
 
 
 
